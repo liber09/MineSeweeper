d30601b89502bf7f89c48d4951f603813a315203
liber09 <113246195+liber09@users.noreply.github.com>
2022-10-25 17:41:37 +0200

    moved some parts of gameloop to own methods and added some comments

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,3 @@
-
-import java.util.IllegalFormatCodePointException;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -18,7 +15,6 @@ public class Game {
         int difficulty;
         int round = 1;
 
-
         System.out.println("              "+TEXT_RED+"                 *    *       ; * . \n"+ TEXT_RESET+
                     "Hello! \uD83D\uDCA3 Welcome to"+ TEXT_BOLD+ TEXT_RED+"        * MI,  SW  **,  ER\n"+
                     "                       "+TEXT_RED+"  * '; "+TEXT_YELLOW+"NE *  EE"+TEXT_RED+"P' *  ;\n"+
@@ -35,8 +31,6 @@ public class Game {
                     "              OOOO OOOOOOOOOO  )\n" +
                     "                OOOOOOOOOO  )\n"+TEXT_RESET);
 
-
-
         do {
             boardSize = chooseLayout();
             difficulty = chooseDifficulty();
@@ -44,34 +38,26 @@ public class Game {
             backendBoard = new Board(boardSize, BoardType.BackendBoard);
             playerBoard = new Board(boardSize, BoardType.PlayerBoard);
             backendBoard.setUpBackendBoard(difficulty);
-            backendBoard.printBoard();
             playerBoard.printBoard();
-            /*backendBoard.setTotalMinesFromStart(boardSize,difficulty); */
 
             System.out.println("Get ready for round " + round + "!");
             gameLoop();
-
             round++;
-
-
-
         }
         while (playAgain());
     }
     private void gameLoop() {
-
         Scanner input = new Scanner(System.in);
         String[] currentInput;
-        int[] coordinates = new int[2];
+        int[] coordinates;
 
         String wannaPlaceFlag;
         boolean gaveUp = false;
-        int hint = 3;
+        int hint = 3; //Player is given three hint possibilities each round
         // Outer loop, runs for each move the player makes
         while (true) {
             printNumberOfMinesAndMarkedMines();
             // Inner loop, runs until the player enters correct input
-
             while (true) {
                 System.out.println(getInstructions(hint));
 
@@ -86,52 +72,34 @@ public class Game {
                     gaveUp = true;
                     break;
                 }
-                currentInput = rawInput.split(" ");
-                // ok den sätter första på index 1 andra på index2 så har jag två strings
-                if(currentInput.length == 1){
-                    System.out.println("Input is needed, please try again");
-                    continue;
-                }
-                wannaPlaceFlag = currentInput[0].substring(0, 1);
-
-                if (wannaPlaceFlag.equalsIgnoreCase("F")) {
-                    try {
-                        coordinates[0] = Integer.parseInt(currentInput[0].substring(1));
-                        coordinates[1] = Integer.parseInt(currentInput[1]);
-                        playerBoard.placeFlag(coordinates[0], coordinates[1]);
-                        playerBoard.printBoard();
-
-                    } catch (NumberFormatException n) {
-                        System.out.println("Please enter co-ordinates (row and column) with just a space in between.");
-
-                    } catch (IndexOutOfBoundsException i) {
-                        System.out.println("Please enter TWO numbers; row and column.");
+                try{
+                    currentInput = rawInput.split(" ");
+                    if (currentInput.length == 1){
+                        System.out.println("Input is needed, please try again");
+                        continue;
                     }
-
-                } else {
-                    try {
-                        for (int i = 0; i < 2; i++) {
-                            coordinates[i] = Integer.parseInt(currentInput[i]);
-                        }
-                            backendBoard.revealEmptySquares(coordinates[0], coordinates[1], playerBoard);
-                            playerBoard.printBoard();
-                            if (playerBoard.isSquareFlag(coordinates[0], coordinates[1])){
-                                System.out.println("There is a flag already on this position, please try another or remove flag first.");
-                                break;
-                            }else{
-                                if (backendBoard.checkIfMine(coordinates[0], coordinates[1])) {
-                                    gameOver(coordinates[0], coordinates[1]);
-                                    return;
-                                }
+                    wannaPlaceFlag = currentInput[0].substring(0, 1);
+                    if (wannaPlaceFlag.equalsIgnoreCase("F")) {
+                        placeFlag(currentInput);
+                    } else {
+                        coordinates = convertUserInput(currentInput);
+                        openSquare(coordinates);
+                        if (playerBoard.isSquareFlag(coordinates[0], coordinates[1])){
+                            System.out.println("There is a flag already on this position, please try another or remove flag first.");
+                            break;
+                        } else {
+                            if (backendBoard.checkIfMine(coordinates[0], coordinates[1])) {
+                                gameOver(coordinates[0], coordinates[1]);
+                                return;
                             }
-                    } catch (NumberFormatException n) {
-                        System.out.println("Please enter co-ordinates (row and column) with just a space in between.");
-                    } catch (IndexOutOfBoundsException i) {
-                        System.out.println("Please enter TWO numbers; row and column.");
+                        }
                     }
-
+                } catch (NumberFormatException n) {
+                    System.out.println("Please enter co-ordinates (row and column) with just a space in between.");
+                } catch (IndexOutOfBoundsException i) {
+                    System.out.println("Please enter TWO numbers; row and column.");
                 }
-                if(playerBoard.checkWin(backendBoard.getTotalMinesFromStart(), backendBoard)) {
+                if (playerBoard.checkWin(backendBoard.getTotalMinesFromStart(), backendBoard)) {
                     System.out.println(TEXT_YELLOW +"Congratulations! You made it!"+TEXT_RESET);
                     wins++;
                     System.out.println("Your total wins are: " + wins);
@@ -139,12 +107,43 @@ public class Game {
                 }
             }
 
-            if(gaveUp){
+            if (gaveUp){
                 break;
             }
         }
     }
 
+    //Takes the user input string array and converts it to int array instead.
+    private int[] convertUserInput(String[] input){
+        int[] coordinates = new int[2];
+        for (int i = 0; i < 2; i++) {
+            coordinates[i] = Integer.parseInt(input[i]);
+        }
+        return coordinates;
+    }
+
+    //calls revelSquares to open one or more squares and prints the board.
+    private void openSquare(int[] coordinates){
+        backendBoard.revealEmptySquares(coordinates[0], coordinates[1], playerBoard);
+        playerBoard.printBoard();
+    }
+
+    //Place a flag at given coordinates and print the board
+    private void placeFlag(String[] input){
+        int[] coordinates = new int[2];
+        try {
+            coordinates[0] = Integer.parseInt(input[0].substring(1));
+            coordinates[1] = Integer.parseInt(input[1]);
+            playerBoard.placeFlag(coordinates[0], coordinates[1]);
+            playerBoard.printBoard();
+        } catch (NumberFormatException n) {
+            System.out.println("Please enter co-ordinates (row and column) with just a space in between.");
+        } catch (IndexOutOfBoundsException i) {
+            System.out.println("Please enter TWO numbers; row and column.");
+        }
+    }
+
+    //Depending on if there are hints left we return different instruction text
     private String getInstructions(int hintsLeft){
         String instructions;
         if (hintsLeft > 0){
@@ -166,6 +165,7 @@ public class Game {
         return instructions;
     }
 
+    //What size of board does the player want, 6x6 to 40x40 is possible.
     public int chooseLayout() {
         System.out.println(
                 " How large do you want your board to be?\n " +
@@ -194,6 +194,7 @@ public class Game {
         return scale; // boardSize
     }
 
+    //What difficulty does the player want, options are easy(10 percent mines), medium(15 percent mines) hard(20 percent mines)
     protected int chooseDifficulty() {
         System.out.println("GREAT CHOICE! What level of difficulty do you choose?\n" +
                 "Please press "+TEXT_YELLOW +"\"e\" for easy, \"m\" for medium and \"h\" for hard!"+TEXT_RESET);
@@ -226,29 +227,22 @@ public class Game {
 
         return difficulty;
     }
-
+    //Checks if the player wants to play the game again
     public boolean playAgain() {
-
-
         while(true){
             System.out.println("Wanna play again? Type "+TEXT_YELLOW+"\"c\" for continue,"+TEXT_RED+" type \"q\" to quit"+TEXT_RESET);
             String playAgain = sc.nextLine();
 
             if (playAgain.equalsIgnoreCase("c")) {
-               // playerBoard.resetBoard();
-               // backendBoard.resetBoard(); behövs inte
                 return true;
-
             } else if (playAgain.equalsIgnoreCase("q")) {
                 System.out.println("See you around! Bye, bye!");
-
                 return false;
             } else {
                 System.out.println("Not a valid answer");
-
             }
-
-    }}
+        }
+    }
 
     public void gameOver(int x, int y){
         System.out.println(TEXT_RED+"BOOM!! \uD83D\uDCA3" +TEXT_RESET+" X= "+x+" and Y= "+y+" was a mine\n GAME OVER!");
@@ -258,11 +252,5 @@ public class Game {
     public void printNumberOfMinesAndMarkedMines(){
         System.out.println("Number of mines to find "+ backendBoard.getTotalMinesFromStart() + ". You have now marked "+playerBoard.countNumberOfMarkedBombs()+ " suspected mines.");
     }
-
-
     static int wins = 0;
-
     }
-
-
-
