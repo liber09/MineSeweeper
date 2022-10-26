2cd4e0568e401c005e6d2340d33b2347ecd127c4
Tobias Sörensson <tobbesoren@gmail.com>
2022-10-26 12:53:17 +0200

    Renaming andrefactoring Board class

--- src/Board.java
+++ src/Board.java
@@ -2,39 +2,38 @@ import java.util.Random;
 public class Board {
     protected int boardSize; //The size of the board.
     private String[][] board; //Contains the board.
-    private int totalMinesFromStart;
+    private int totalNumberOfBombs;
     private BoardType type;
     protected final String UNKNOWN = TEXT_GREEN+"\u25af"+TEXT_RESET; //Hidden square
-    private final String MINE = TEXT_RED+"\u2638"+TEXT_RESET; //Mine square
+    private final String BOMB = TEXT_RED+"\u2638"+TEXT_RESET; //Mine square
     private final String FLAG = TEXT_LIGHT_RED +"\u2691"+TEXT_RESET; //User suspects mine square
     protected final String EMPTY = " "; //Empty square
     public static final String TEXT_RED = "\u001B[31m";
     public static final String TEXT_LIGHT_RED = "\u001B[91m";
     public static final String TEXT_GREEN = "\u001B[32m";
     public static final String TEXT_RESET = "\u001B[0m";
+
     /*
-        Board constructor.
-        Takes boardSize as parameter and fills
-        every square with UNKNOWN sign.
-
-
-     */
+      Board constructor.
+      Takes boardSize and BoardType as parameter and fills
+      every square with UNKNOWN or EMPTY sign, depending on BoardType.
+    */
     public Board(int boardSize, BoardType type) {
         this.boardSize = boardSize;
         this.type = type;
         createBoard(type);
     }
 
-    //return the number of mines on the board.
-    public int getTotalMinesFromStart(){
-        return this.totalMinesFromStart;
+    //return the number of bombs on the board.
+    public int getTotalNumberOfBombs(){
+        return this.totalNumberOfBombs;
     }
-    //Count how many mines the player has marked on the board and return number
-    public int countNumberOfMarkedBombs(){
+    //Count how many flags the player has placed on the board and return number
+    public int countFlags(){
         int flagCounter = 0;
-        for(int i = 0;i<boardSize;i++){
-            for(int j = 0;j<boardSize;j++){
-                if(board[i][j].equals(FLAG)){
+        for(int row = 0;row<boardSize;row++){
+            for(int column = 0;column<boardSize;column++){
+                if(board[row][column].equals(FLAG)){
                     flagCounter++;
                 }
             }
@@ -42,43 +41,209 @@ public class Board {
         return flagCounter;
     }
 
-    //Checks a square to see if it contains a flag, if so return true, otherwise return false.
-
-    public Boolean isSquareFlag(int x, int y){
-        if (checkSquare(x,y).equals(FLAG)){
-            return true;
-        }
-        return false;
+    //Takes row and y coordinates and returns the value of that square
+    public String checkSquare(int row, int column){
+        return board[row][column];
     }
-    //Place a flag if square is unknown or remove flag if there is a flag on the position. Place on playerBoard and print the board.
-    public void placeFlag(int x, int y){
-        if(board[x][y].equals(UNKNOWN)){
-            board[x][y] = FLAG;
+
+
+    //Checks a square to see if it contains a flag, if so return true, otherwise return false.
+    public Boolean isSquareFlag(int row, int column){
+        return checkSquare(row,column).equals(FLAG);
+    }
+
+
+    public boolean checkIfBomb(int row, int column) {
+        return checkSquare(row,column).equals(BOMB);
+    }
+
+
+    //Place a flag if square is unknown or remove flag if there is a flag on the position. Place on playerBoard.
+    public void placeFlag(int row, int column){
+        if(board[row][column].equals(UNKNOWN)){
+            board[row][column] = FLAG;
         }
-        else if(board[x][y].equals(FLAG)){
-            board[x][y] = UNKNOWN;
+        else if(board[row][column].equals(FLAG)){
+            board[row][column] = UNKNOWN;
         }
 
     }
     public void createBoard(BoardType type) {
         board = new String[boardSize][boardSize];
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
+        for (int row = 0; row < boardSize; row++) {
+            for (int column = 0; column < boardSize; column++) {
                 if (type == BoardType.PlayerBoard) {
-                    board[i][j] = UNKNOWN;
+                    board[row][column] = UNKNOWN;
                 }
                 if (type == BoardType.BackendBoard) {
-                    board[i][j] = EMPTY;
+                    board[row][column] = EMPTY;
                 }
 
             }
         }
     }
 
+
     /*
-        Prints the board. A couple of loops
-        printing different parts of the board.
+    Takes an int, difficulty, that the user provides.
+    Sets up backendBoard with mines and numbers, using placeBombs and countBombs
      */
+    public void setUpBackendBoard(int difficulty) {
+        int totalSquares = boardSize * boardSize;
+        totalNumberOfBombs = totalSquares * difficulty / 100;
+        placeBombs();
+
+        for (int row = 0; row < boardSize; row++) {
+            for (int column = 0; column < boardSize; column++) {
+                int surroundingMines = countBombs(row, column);
+                if (surroundingMines != 0 && board[row][column] == EMPTY) {
+                    board[row][column] = String.valueOf(surroundingMines);
+                }
+            }
+        }
+    }
+
+
+    /*
+    Takes two ints, representing index for row and column in the board
+    Counts the bombs on the (up to) eight squares surrounding the chosen square.
+    Returns the result as an int.
+    */
+    public int countBombs(int row, int column) {
+
+        int numberOfBombs = 0;
+
+        for (int y = row - 1; y <= row + 1; y++) {
+            for (int x = column - 1; x <= column + 1; x++) {
+
+                // If we check a square next to the edge, we will get an IndexOutOfBondsException,
+                // so we just catch it and move on.
+                try {
+                    // We don't check the middle square!
+                    if (!(y == row && x == column)) {
+                        if (board[y][x] == BOMB) {
+                            numberOfBombs++;
+                        }
+                    }
+                } catch (IndexOutOfBoundsException i) {
+                    /*
+                    Nothing to see here, just carry on with your day!
+                    */
+                }
+            }
+        }
+        return numberOfBombs;
+    }
+
+
+    //Place bombs
+    public void placeBombs() {
+        Random ran = new Random();
+
+        for (int i = 0; i < totalNumberOfBombs; i++) {
+            int row = ran.nextInt(boardSize);
+            int column = ran.nextInt(boardSize);
+            if (!checkIfBomb(row,column)){ // Changed from validBombPlacement to !checkIfBomb - removed validBombPlacement.
+                board[row][column] = BOMB;
+            }else{
+                i--;
+            }
+        }
+    }
+
+
+    /*
+    Used in backendBoard to reveal empty squares and add them to playerBoard. It also adds the border numbers.
+     */
+    public void revealSquares(int row, int column, Board playerBoard) {
+        // first, we check the bounderies
+        if(row < 0 || row > boardSize - 1 || column < 0 || column > boardSize - 1) {
+            return;
+        }
+        // if all squares are checked but the bombs
+        //if(totalMinesFromStart+1==playerBoard.UNKNOWN.length()){
+          //  System.out.println("HINT: There are only "+totalMinesFromStart+" bombs on the field");
+
+       // }
+        // Here we check if the square is empty and not already checked.
+        if(board[row][column] == EMPTY && playerBoard.board[row][column] == UNKNOWN) {
+            // If so, we update the square in playerBoard...
+            playerBoard.board[row][column] = EMPTY;
+            // ...and check the surrounding squares by four recursive calls!
+            revealSquares(row - 1, column, playerBoard);
+            revealSquares(row + 1, column, playerBoard);
+            revealSquares(row, column - 1, playerBoard);
+            revealSquares(row, column + 1, playerBoard);
+        // If the square isn't EMPTY, but unchecked, we just uncover it
+        } else if(playerBoard.board[row][column] == UNKNOWN) {
+            playerBoard.board[row][column] = board[row][column];
+        }
+    }
+
+    /*
+        Checks if the player has won by looping through the board
+        and count all UNKNOWN squares.
+        If they are more than the totalMinesFromStart
+        the player has not won. If they are equal, the player has cleared all unknown squares without mines and won the game.
+     */
+    public boolean checkWin(int totalMineCount, Board backendBoard) {
+        int unKnownCounter = 0;
+        int flagCounter = 0;
+        boolean hasPlayerWon = false;
+        boolean hasFlaggedFalseSquare = false;
+        for (int row = 0; row < boardSize; row++) {
+            for (int column = 0; column < boardSize; column++) {
+                if (board[row][column].equals(UNKNOWN)) {
+                    unKnownCounter++;
+                }
+                if (board[row][column].equals(FLAG) && backendBoard.board[row][column].equals(BOMB)){
+                    flagCounter++;
+                } else if(board[row][column].equals(FLAG) && backendBoard.board[row][column].equals(EMPTY)) {
+                    hasFlaggedFalseSquare = true;
+                    return false;
+
+                }
+
+        } return hasFlaggedFalseSquare;}//Winning by marking all the right bombs!
+
+
+
+        if (!hasFlaggedFalseSquare &&((unKnownCounter+flagCounter) == totalMineCount || flagCounter == totalMineCount)) {
+
+            System.out.println("All Bombs found!!");
+
+            hasPlayerWon = true;
+
+        }
+        return hasPlayerWon;
+    }
+
+
+
+
+
+    public void hint(Board PlayerBoard ){
+        Random ran = new Random();
+
+        int row = ran.nextInt(boardSize);
+        int column = ran.nextInt(boardSize);
+
+
+        if (checkIfBomb(row, column)|| !PlayerBoard.checkSquare(row,column).equals(UNKNOWN))
+        {
+            hint(PlayerBoard);
+
+        }
+
+        else {
+            revealSquares(row,column, PlayerBoard);}} // does not work properly when all mines are flagged but one is a false flag
+    //it casts an error
+
+
+    /*
+      Prints the board. A couple of loops
+      printing different parts of the board.
+    */
     public void printBoard() {
         //Print column headers
         for (int i = 0; i < boardSize; i++) {
@@ -152,180 +317,4 @@ public class Board {
             }
         }
     }
-
-    /*
-    Takes an int, difficulty, that the user provides.
-    Sets up backendBoard with mines and numbers, using placeBombs and countBombs
-     */
-    public void setUpBackendBoard(int difficulty) {
-        int totalSquares = boardSize*boardSize;
-        totalMinesFromStart = totalSquares * difficulty;
-        totalMinesFromStart = totalMinesFromStart/100; ///
-        placeBombs();
-        for (int row = 0; row < boardSize; row++) {
-            for (int column = 0; column < boardSize; column++) {
-                int surroundingMines = countBombs(row, column);
-                if (surroundingMines != 0 && board[row][column] == EMPTY) {
-                    board[row][column] = String.valueOf(surroundingMines);
-                }
-            }
-        }
-    }
-
-
-
-    /*
-    Takes two ints, representing index for row and column in the board
-    Counts the bombs on the (up to) eight squares surrounding the chosen square.
-    Returns the result as an int.
-    */
-    public int countBombs(int row, int column) {
-
-        int numberOfBombs = 0;
-
-        for (int y = row - 1; y <= row + 1; y++) {
-            for (int x = column - 1; x <= column + 1; x++) {
-
-                // If we check a square next to the edge, we will get an IndexOutOfBondsException,
-                // so we just catch it and move on.
-                try {
-
-                    if (!(y == row && x == column)) {
-                        if (board[y][x] == MINE) { // << Fixed it!
-                            numberOfBombs++;
-                        }
-                    }
-                } catch (IndexOutOfBoundsException i) {
-                    /*
-                    Nothing to see here, just carry on with your day!
-                    */
-                }
-            }
-        }
-        return numberOfBombs;
-    }
-
-
-    //Place bombs
-    public void placeBombs() {
-        Random ran = new Random();
-
-        for (int i = 0; i < totalMinesFromStart; i++) {
-            int x = ran.nextInt(boardSize);
-            int y = ran.nextInt(boardSize);
-            if (validBombPlacement(x,y)){
-                board[x][y] = MINE;
-            }else{
-                i--;
-            }
-        }
-    }
-    // Check so that there is no mine in this position already
-    public boolean validBombPlacement(int x, int y)
-    { if (board[x][y].equals(MINE)){
-        return false;
-    }  return true;
-
-    }
-    //Takes x and y coordinates and returns the value of that square
-    public String checkSquare(int x, int y){
-        return board[x][y];
-    }
-
-    /*
-    Used in backendBoard to reveal empty squares and add them to playerBoard. It also adds the border numbers.
-     */
-    public void revealEmptySquares(int row, int column, Board playerBoard) {
-        // first, we check the bounderies
-        if(row < 0 || row > boardSize - 1 || column < 0 || column > boardSize - 1) {
-            return;
-        }
-        // if all squares are checked but the bombs
-        //if(totalMinesFromStart+1==playerBoard.UNKNOWN.length()){
-          //  System.out.println("HINT: There are only "+totalMinesFromStart+" bombs on the field");
-
-       // }
-        // Here we check if the square is empty and not already checked.
-        if(board[row][column] == EMPTY && playerBoard.board[row][column] == UNKNOWN) {
-            // If so, we update the square in playerBoard...
-            playerBoard.board[row][column] = EMPTY;
-            // ...and check the surrounding squares by four recursive calls!
-            revealEmptySquares(row - 1, column, playerBoard);
-            revealEmptySquares(row + 1, column, playerBoard);
-            revealEmptySquares(row, column - 1, playerBoard);
-            revealEmptySquares(row, column + 1, playerBoard);
-        // If the square isn't EMPTY, but unchecked, we just uncover it
-        } else if(playerBoard.board[row][column] == UNKNOWN) {
-            playerBoard.board[row][column] = board[row][column];
-        }
-    }
-
-    /*
-        Checks if the player has won by looping through the board
-        and count all UNKNOWN squares.
-        If they are more than the totalMinesFromStart
-        the player has not won. If they are equal, the player has cleared all unknown squares without mines and won the game.
-     */
-    public boolean checkWin(int totalMineCount, Board backendBoard) {
-        int unKnownCounter = 0;
-        int flagCounter = 0;
-        boolean hasPlayerWon = false;
-        boolean hasFlaggedFalseSquare = false;
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                if (board[i][j].equals(UNKNOWN)) {
-                    unKnownCounter++;
-                }
-                if (board[i][j].equals(FLAG) && backendBoard.board[i][j].equals(MINE)){
-                    flagCounter++;
-                } else if(board[i][j].equals(FLAG) && backendBoard.board[i][j].equals(EMPTY)) {
-                    hasFlaggedFalseSquare = true;
-                    return false;
-
-                }
-
-        } return hasFlaggedFalseSquare;}//Winning by marking all the right bombs!
-
-
-
-        if (!hasFlaggedFalseSquare &&((unKnownCounter+flagCounter) == totalMineCount || flagCounter == totalMineCount)) {
-
-            System.out.println("All Bombs found!!");
-
-            hasPlayerWon = true;
-
-        }
-        return hasPlayerWon;
-    }
-
-
-
-
-
-    public void hint(Board PlayerBoard ){
-        Random ran = new Random();
-
-        int x = ran.nextInt(boardSize);
-        int y = ran.nextInt(boardSize);
-
-
-        if (checkIfMine(x, y)|| !PlayerBoard.checkSquare(x,y).equals(UNKNOWN))
-        {
-            hint(PlayerBoard);
-
-        }
-
-        else {revealEmptySquares(x,y, PlayerBoard);}} // does not work properly when all mines are flagged but one is a false flag
-    //it casts an error
-
-    //Call createNewBoard to reset the gameBoard, use boardType to get correct initial layout.
-     // public void resetBoard() {
-        //createBoard(this.type);
-
-    // }
-
-    public boolean checkIfMine(int x, int y) {
-        return checkSquare(x,y).equals(MINE);
-    }
-
 }
